d1f99ff4a8da2e23b857e0595ab953485b9b18c5
Abbuthahir <abbuthahir04@gmail.com>
2023-09-15 11:33:49 +0530

    15/09/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, PickerButton, PickerOptions } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  percentageValue!: any;
+  canAdd = false;
+  maxDate!: string;
+  
+  async canDismiss(data?: any, role?: string) {
+    return role !== 'gesture';
+  }
+  constructor(private modalCtrl: ModalController) {
+    this.maxDate = this.getCurrentDate();
+  }
 
-  constructor() {}
+  openModal() { }
+  updateAddButtonState() {
+    this.canAdd = /^0\.\d+$/.test(this.percentageValue);
+  }
 
+  getCurrentDate(): string {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const day = currentDate.getDate();
+
+    return `${year}-${this.padNumber(month)}-${this.padNumber(day)}`;
+  }
+
+  padNumber(num: number): string {
+    return num.toString().padStart(2, '0');
+  }
+
+  ngOnInit(): void {
+
+  }
 }
